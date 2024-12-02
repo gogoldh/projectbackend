25fa2d850d01a2e15f2a6e019f6452383d58b2e5
gogoldh <r0987151@student.thomasmore.be>
2024-12-02 20:23:52 +0100

    add server test file

---
+++ plswerk.php
@@ -0,0 +1,15 @@
+<?php
+$servername = 'alienrides.mysql.database.azure.com';
+$username = 'alienrides@alienrides';
+$password = 'aB3$XyZ9!qP&7*rT@1n';
+$database = 'projectbackend';
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>  
