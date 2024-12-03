9ac2ee4c28a758fd76dc63c37e3278073be7d2b0
gogoldh <r0987151@student.thomasmore.be>
2024-12-03 15:09:27 +0100

    fix:deploy

--- classes/Db.php
+++ classes/Db.php
@@ -1,24 +1,28 @@
 <?php
-// class Db {
-//     private static $conn = null;
 
-//     public static function getConnection(){
-//         if (self::$conn == null) {
-//             self::$conn = new PDO('mysql:host=localhost;dbname=projectbackend', 'root', '');
-//         } 
-//         return self::$conn;
-//     }
-// }
+class Db {
+    private static $conn = null;
 
+    public static function getConnection() {
+        // Db::getConnection();
+        if (self::$conn == null) {
+            try {
+                $pathToSSL = './cacert.pem';
+                $options = array(PDO::MYSQL_ATTR_SSL_CA => $pathToSSL);
 
-$dbHost = "alienrides.mysql.database.azure.com";
-$dbUser = "alienrides@alienrides";
-$dbPassword = "aB3$XyZ9!qP&7*rT@1n";
-$dbName = "database_name";
-$conn = new PDO($dbHost, $dbUser, $dbPassword, $dbName);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+                $host = 'alienrides.mysql.database.azure.com';
+                $db = 'alienridesdb';
+                $user = 'alienrides';
+                $pass = 'aB3$XyZ9!qP&7*rT@1n';
+                self::$conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass, $options);
+                self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                // Handle the error, for example, log it and/or display a user-friendly message
+                error_log("Connection failed: " . $e->getMessage());
+                die("Database connection failed. Please try again later.");
+            }
+        }
+        return self::$conn;
+    }
 }
-echo "Connected successfully";
 ?>
